c10ea7ff4dce95a89029151c60339a63aa4c2fd9
Warot Leopairote <warot@Warots-MacBook-Pro.local>
2018-10-25 11:51:30 +0700

    Add untest database connected

--- server/main.go
+++ server/main.go
@@ -5,18 +5,32 @@ import (
 	"log"
 	"net/http"
 
+	. "./config"
+	. "./dao"
+	. "./models"
 	"github.com/gorilla/mux"
 )
 
+var config = Config{}
+var dao = ProductsDAO{}
+
 func SearchProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
+}
+
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	dao.Connect()
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", SearchProduct).Methods("GET")
 
-	if err := http.ListenAndServe(":80", r); err != nil {
+	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
 }
