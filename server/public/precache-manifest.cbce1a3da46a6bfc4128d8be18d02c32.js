a541b767281ad6fea151f802c7fde5ec9948f055
Alongkorn Chetasumon <draood@gmail.com>
2018-10-25 12:09:58 +0700

    add sample static files

---
+++ server/public/precache-manifest.cbce1a3da46a6bfc4128d8be18d02c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed420d14b5bced0c92b5",
+    "url": "/static/js/main.ed420d14.chunk.js"
+  },
+  {
+    "revision": "6d8737b80ad5e86da57a",
+    "url": "/static/js/1.6d8737b8.chunk.js"
+  },
+  {
+    "revision": "ed420d14b5bced0c92b5",
+    "url": "/static/css/main.4f47b100.chunk.css"
+  },
+  {
+    "revision": "dfe7b36eff6235f9631b78ab5b7a4728",
+    "url": "/index.html"
+  }
+];
